0b1e788818af08bfc4ac1fc451c74adf08649d81
klesio25 <klesio.k9@gmail.com>
2021-10-08 17:21:17 -0300

    added new contents

---
+++ 2-variables.php
@@ -0,0 +1,14 @@
+<?php
+
+$nada = null;
+$boolean = true;
+$inteiro = 1;
+$real = 3.14;
+$text = "UTFP";
+
+
+echo ($nada);
+var_dump($text);
+define('Minute', 60);
+define('Hora', Minute * 60);
+echo ("1.2 um dia tem " . (24 * Hora) . ' segundos\n\n');
