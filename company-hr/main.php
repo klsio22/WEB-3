90939a7b7917d76a3b33dbf935ee99f600faab82
klesio <klesio.dev@gmail.com>
2023-03-14 08:58:51 -0300

    save new files

---
+++ company-hr/main.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Application\Model; //procura na mesma pasta
+
+
+/* require 'file.php' -> só executa caso encontre o local arquivo 
+ele para a aplicação caso não encontre 
+
+include '..file.php -> procura a pasta caso não encotre ele passa direto'
+include.once '..file.php'-> inclui somente uma vez 
+*/
+
+require_once 'Person.php';
